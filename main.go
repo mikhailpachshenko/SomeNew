72d4e50090f1ea5b2d593a43c07f749c5dd899cd
mikhailpachshenko <mikhailpachshenko@gmail.com>
2022-07-11 23:17:42 +0600

    commitII

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ func main() {
 	a := 10
 	b := 20
 	c, d := func(a, b int) (int, int) {
-		res := a + b // first value
-		resII := a * b
+		res := a + b   // first value
+		resII := a * b // second value
 		return res, resII
 	}(a, b)
 
