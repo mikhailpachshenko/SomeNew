6960630da67807a5776f52bd36a21ed5e679ff82
mikhailpachshenko <mikhailpachshenko@gmail.com>
2022-07-11 23:18:26 +0600

    commentIII

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ func main() {
 	a := 10
 	b := 20
 	c, d := func(a, b int) (int, int) {
-		res := a + b   // first value
-		resII := a * b // second value
-		return res, resII
+		res := a + b      // first value
+		resII := a * b    // second value
+		return res, resII // return two value
 	}(a, b)
 
 	fmt.Println("multiplication:", d)
