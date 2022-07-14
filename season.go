e8933e7e00a9c1891bb06bc391449e33e5e25ffb
mikhailpachshenko <mikhailpachshenko@gmail.com>
2022-07-14 13:49:31 +0600

    new package season.go

---
+++ season.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var seasonSelect string
+	fmt.Scan(&seasonSelect)
+	seasonSelect = strings.ToLower(seasonSelect)
+
+	switch seasonSelect {
+	case "декабрь", "январь", "февраль":
+		fmt.Printf("%s%s является месяцем Зимы\n", strings.ToUpper(seasonSelect[:2]), seasonSelect[2:])
+	case "март", "апрель", "май":
+		fmt.Printf("%s ялвяется месяцем Весны\n", strings.ToUpper(seasonSelect))
+	case "июнь", "июль", "август":
+		fmt.Printf("%s ялвяется месяцем Лета\n", seasonSelect)
+	case "сентябрь", "октябрь", "ноябрь":
+		fmt.Printf("%s является месяцем Осени\n", seasonSelect)
+	}
+}
