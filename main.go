7eba2e0485b2968cdc24af24bd9e8af0e6f08456
mikhailpachshenko <mikhailpachshenko@gmail.com>
2022-07-18 15:31:17 +0600

    restore main.go from .gitignore and fomrating main.go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := 20
+	c, d := func(a, b int) (int, int) {
+		res := a + b      // first value
+		resII := a * b    // second value
+		return res, resII // return two value
+	}(a, b)
+
+	fmt.Println("multiplication:", d)
+	fmt.Println("sum:", c)
+}
